ab995fa49db672fec061d3fd47cfcce1e774d623
Caroline <cschinzel@gmail.com>
2012-12-19 23:12:11 +0100

    [Dev du menu] Menu dynamique

--- carolive-hispanoamerica/wp-content/themes/carolive/header.php
+++ carolive-hispanoamerica/wp-content/themes/carolive/header.php
@@ -8,73 +8,24 @@
 		<li class="universe">
 			<a href="<?php bloginfo('url'); ?>">Home</a>
 		</li>
-		<li class="universe">
-			<a href="getready.html">Préparatif</a>
-			<ul>
-				<li>
-					<a href="budget.html">Budget</a>
-				</li>
-				<li>
-					<a href="findasso.html">Trouver des associations</a>
-				</li>
-				<li>
-					<a href="insurance.html">Choisir une assurance</a>
-				</li>
-				<li>
-					<a href="leavejob.html">Quitter son travail</a>
-				</li>
-				<li>
-					<a href="miscellaneous.html">Divers</a>
-				</li>
-			</ul>
-		</li>
-		<li class="universe">
-			<a href="trip.html">Notre Voyage</a>
-			<ul>
-				<li>
-					<a href="costarica.html">Costa Rica</a>
-				</li>
-				<li>
-					<a href="peru.html">Pérou</a>
-				</li>
-				<li>
-					<a href="equator.html">Equateur</a>
-				</li>
-				<li>
-					<a href="bolivia.html">Bolivie</a>
-				</li>
-				<li>
-					<a href="chile.html">Chile</a>
-				</li>
-				<li>
-					<a href="patagonia.html">Patagonie</a>
-				</li>
-				<li>
-					<a href="argentina.html">Argentine</a>
-				</li>
-			</ul>
-		</li>
-		<li class="universe">
-			<a href="whoisitabout.html">Qui sommes-nous ?</a>
-			<ul>
-				<li>
-					<a href="oliver.html">CV d'Olivier</a>
-				</li>
-				<li>
-					<a href="caroline.html">CV de Caroline</a>
-				</li>
-			</ul>
-		</li>
-		<li class="universe">
-			<a href="help.html">Soutenez-nous</a>
-			<ul>
-				<li>
-					<a href="encouragment.html">Encouragements</a>
-				</li>
-				<li>
-					<a href="money.html">Soutien financier</a>
-				</li>
-			</ul>
-		</li>
+    <?php
+    $universes = get_categories('orderby=id&exclude=1,3,4&hide_empty=0');
+    foreach ($universes as $universe) {
+      if ($universe->parent < 1) {
+    ?>
+        <li class="universe">
+          <a href="<?php echo get_category_link( $universe->term_id ); ?>"><?php echo $universe->name ?></a>
+          <ul>
+            <?php
+            $categories = get_categories('orderby=id&child_of='.$universe->term_id.'&hide_empty=0');
+            foreach ($categories as $category) {
+            ?>
+              <li>
+                <a href="<?php echo get_category_link( $category->term_id ); ?>"><?php echo $category->name ?></a>
+              </li>
+            <?php } ?>
+          </ul>
+        </li>
+    <?php } } ?>
 	</ul>
 </nav>

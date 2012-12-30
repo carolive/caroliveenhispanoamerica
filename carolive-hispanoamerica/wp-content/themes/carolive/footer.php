97ec8a7db7f1decf96cd2da6de8b473e84c9f753
Caroline <cschinzel@gmail.com>
2012-12-30 18:57:30 +0100

    [Mentions legales] Ajout du lien vers la page dans le footer

--- carolive-hispanoamerica/wp-content/themes/carolive/footer.php
+++ carolive-hispanoamerica/wp-content/themes/carolive/footer.php
@@ -1,7 +1,7 @@
 <div id="footer"> <!-- comme le header, j'enleve le footer du "bodysite" afin qu'il prenne toute la largeur -->
 	<div class="footer_ctnr"> <!-- comme le header, je rajoute une div pour centrer le contenu -->
 		<ul>
-			<li class="backtotop"><a href="#ceh">Haut de page</a></li>
+			<li class="contact"><a href="<?php bloginfo('url'); ?>/contact">Contact</a></li>
 			<li class="copyright">&copy; copyright 2013 - Carolive En Hispanoamerica</li>
 		</ul>
 	</div>
---
+++ carolive-hispanoamerica/wp-content/themes/carolive/page-contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head profile="http://gmpg.org/xfn/11">
+  <?php include 'head-commons.php'; ?>
+</head>
+<body class="no-customize-support">
+<?php wp_customize_support_script(); ?>
+
+<?php get_header(); ?>
+  
+  <div class="bodysite">
+    <div class="container main"> <!-- container : la grille -->
+      
+      <div class="span-24 last left_column">
+        <div id="caroliveposts" class="block">
+          <div class="block_title">
+            <h2><span>Contactez</span>-nous</h2>
+          </div>
+          <div class="block_content">
+            <?php while ( have_posts() ) : the_post(); ?>
+
+              <?php the_content(); ?>
+
+            <?php endwhile; // end of the loop. ?>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  </div>    
+  
+  <?php get_footer(); ?>
+</body>
+</html>

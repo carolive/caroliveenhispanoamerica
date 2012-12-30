164a7ef6598c928b8430cb4e411d7b7d549731f9
Caroline <cschinzel@gmail.com>
2012-12-30 19:32:25 +0100

    [Mention legale] Stylisation de la page contact

--- carolive-hispanoamerica/wp-content/themes/carolive/page-contact.php
+++ carolive-hispanoamerica/wp-content/themes/carolive/page-contact.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head profile="http://gmpg.org/xfn/11">
   <?php include 'head-commons.php'; ?>
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/contact.css" media="screen" type="text/css" />
 </head>
 <body class="no-customize-support">
 <?php wp_customize_support_script(); ?>
@@ -12,7 +13,7 @@
     <div class="container main"> <!-- container : la grille -->
       
       <div class="span-24 last left_column">
-        <div id="caroliveposts" class="block">
+        <div id="contact" class="block">
           <div class="block_title">
             <h2><span>Contactez</span>-nous</h2>
           </div>

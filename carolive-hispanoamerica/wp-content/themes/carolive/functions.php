408527a7efc542717992bb349fcf3854f50e6b75
Caroline <cschinzel@gmail.com>
2013-01-28 23:43:17 +0100

    [Commentaires] Ajout des commentaires et des layouts pour styliser

--- carolive-hispanoamerica/wp-content/themes/carolive/functions.php
+++ carolive-hispanoamerica/wp-content/themes/carolive/functions.php
@@ -4,4 +4,21 @@ if ( function_exists( 'add_theme_support' ) ) {
 add_theme_support( 'post-thumbnails' );
 }
 
+add_filter('wp_head','sb_force_comment');
+function sb_force_comment( ) {
+  global $withcomments;
+  if(is_category())
+    $withcomments = true; //force to show the comment on category page
+}
+
+function carolive_comment( $comment, $args, $depth ) {
+  $GLOBALS['comment'] = $comment;
+?>
+	<li id="li-comment-<?php comment_ID(); ?>">
+			<div class="comment-content"><?php comment_text(); ?></div>
+  </li>
+
+<?php
+}
+
 ?>
